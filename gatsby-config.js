29b74406463ce7611452030cb5667a9f7abbd7cb
cyng24 <cyng24@gmail.com>
2021-08-14 17:58:31 +0200

    Add gatsby config and package.json

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Settlers of Catan",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+};
