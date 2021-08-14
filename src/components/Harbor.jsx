9a7476328228378dac9c1687e8bb71b4ee5211f7
cyng24 <cyng24@gmail.com>
2021-08-14 17:51:12 +0200

    Add gatsby cloud

--- src/components/Harbor.jsx
+++ src/components/Harbor.jsx
@@ -6,13 +6,12 @@ const Harbor = (props) => {
   return (
     <div>
       <svg width="140" height="140">
+        {/* <rect x="48" y="48" width="35" height="35" stroke="none" fill={propArray[0]} /> */}
         <circle cx="65" cy="65" r="20" stroke="none" fill={propArray[0]} />
-        <text x="55" y="75" fontWeight="bold" fontSize="10px" fontColor="white" fill="black">{propArray[1]}</text>
+        <text x="55" y="70" fontWeight="bold" fontSize="15px" fill="white">{propArray[1]}</text>
       </svg>
     </div>
   )
 }
 
 export default Harbor;
-
-//
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import '../App.css';
 
 const Tile = (props) => {
+  const textX = props.value > 9 ? "48" :"55";
   return (
     <div>
       <svg width="140" height="140">
         <polygon points="0 30 65 0 130 30 130 100 65 130 0 100"stroke="none" fill={props.type}/>
         <circle cx="65" cy="65" r="20" stroke="none" fill="white" />
-        <text x="55" y="75" fontWeight="bold" fontSize="30px" fill="black">{props.value}</text>
+        <text x={textX} y="75" fontWeight="bold" fontSize="30px" fill="black">{props.value}</text>
       </svg>
     </div>
   )
