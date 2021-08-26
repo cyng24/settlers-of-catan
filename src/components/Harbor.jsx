5cdfe5610e25b6eb54d69a3b9c6ae527712027a6
cyng24 <cyng24@gmail.com>
2021-08-26 21:40:05 +0200

    Modify react for gatsby

---
+++ src/components/Harbor.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style.css';
+
+const Harbor = (props) => {
+    let propArray = props.type ? props.type.split('|') : ['white', null];
+  return (
+    <div>
+      <svg width="140" height="140">
+        {/* <rect x="48" y="48" width="35" height="35" stroke="none" fill={propArray[0]} /> */}
+        <circle cx="65" cy="65" r="20" stroke="none" fill={propArray[0]} />
+        <text x="55" y="70" fontWeight="bold" fontSize="15px" fill="white">{propArray[1]}</text>
+      </svg>
+    </div>
+  )
+}
+
+export default Harbor;
---
+++ src/components/Tile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style.css';
+
+const Tile = (props) => {
+  const textX = props.value > 9 ? "48" :"55";
+  return (
+    <div>
+      <svg width="140" height="140">
+        <polygon points="0 30 65 0 130 30 130 100 65 130 0 100"stroke="none" fill={props.type}/>
+        <circle cx="65" cy="65" r="20" stroke="none" fill="white" />
+        <text x={textX} y="75" fontWeight="bold" fontSize="30px" fill="black">{props.value}</text>
+      </svg>
+    </div>
+  )
+}
+
+export default Tile;
